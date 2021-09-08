048f2d4c07bb1fc69b2fa7735913fc07ea06d169
Ayush <ayushthakur3001@gmail.com>
2021-09-08 18:40:03 +0530

    Watch Section Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './index.css'
 import { Landing } from './pages/Landing'
 import { Enjoy } from './pages/Enjoy'
 import { Download } from './pages/Download'
+import { Watch } from './pages/Watch'
 const App = () => {
   return (
     <div className='app'>
       <Landing/>
       <Enjoy/>
       <Download/>
+      <Watch />
     </div>
   )
 }
