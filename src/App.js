e371d70bc6dc88ed95111e6f65027cd2cf8be777
Ayush <ayushthakur3001@gmail.com>
2021-09-08 15:32:21 +0530

    Download Secion Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './index.css'
 import { Landing } from './pages/Landing'
 import { Enjoy } from './pages/Enjoy'
+import { Download } from './pages/Download'
 const App = () => {
   return (
     <div className='app'>
       <Landing/>
       <Enjoy/>
+      <Download/>
     </div>
   )
 }
