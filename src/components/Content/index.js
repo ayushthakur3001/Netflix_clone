e94bbb25c861f90d095e9020af049c10c294fd2e
Ayush <ayushthakur3001@gmail.com>
2021-09-09 19:57:50 +0530

    Refactored Code

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,27 +1,11 @@
 import "./style.scss";
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { EmailInput } from "../MembershipEmail";
 export const Content = () => {
   return (
     <div className="content">
       <p className="heading">Unlimited movies, TV shows and more.</p>
       <h2>Watch anywhere. Cancel anytime.</h2>
-      <h3>
-        Ready to watch? Enter your email to create or restart your membership.
-      </h3>
-      <form>
-      <div className='emailInput'>
-      <input type='email' className='emailField' placeholder='Email address' />
-      <button>
-          Get Started
-          <KeyboardArrowRightIcon/>
-      </button>
-
-      </div>
-
-      </form>
-      
-        
-        
+      <EmailInput/> 
     </div>
   );
 };
