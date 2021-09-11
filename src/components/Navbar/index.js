d96cb77db08ce614ff16324b0c473da7fc506421
Ayush <ayushthakur3001@gmail.com>
2021-09-11 14:57:41 +0530

    Navbar Added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,43 +1,14 @@
 import './style.scss'
 import logo from "../../assets/logo.png"
-import LanguageIcon from '@material-ui/icons/Language';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import { useState } from 'react';
+import { Language } from '../Language'
 export const Navbar = () => {
-    const [lang, setLang] = useState('English')
-    const [langOption, setLangOption] = useState(false)
+    
     return (
         <div className="navBar">
           <img src={logo} alt="..." />
           <div>
-          <div className='changeLang'>
-          <button className='langButton' onClick={()=> setLangOption(!langOption)} >
-            <LanguageIcon className='langIcon' fontSize='small'/>
-              {lang}
-            <ArrowDropDownIcon />
-          </button>
-        {
-            langOption &&
-            <div className='options'>
-              <li 
-              className={ langOption === 'English' ? 'active':'' }
-              onClick={()=>{
-                  setLang("English") 
-                  setLangOption(false)
-              }} 
-              >English</li>
-              <li 
-              className={ langOption === 'हिंदी' ? 'active':'' }
-              onClick={()=>{
-                  setLang("हिंदी") 
-                  setLangOption(false)
-              }}
-              >हिंदी</li>
-          </div>
-        }
-          </div>
+          <Language/>
           <button className='signIn'>Sign In</button>
-          
           </div>
         </div>
     )
