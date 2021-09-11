b68dd19c28c2318369347220e5fbcda176d3d7ec
Ayush <ayushthakur3001@gmail.com>
2021-09-11 15:11:50 +0530

    Footer Section Homepage

---
+++ src/components/Footer/index.js
@@ -0,0 +1,43 @@
+import "./style.scss";
+import { Language } from "../../components/Language";
+const links = [
+  ["FAQ", "Ways to Watch", "Cookie Preferences", "Speed Test"],
+
+  [
+    "Help Centre",
+    "Investor Relations",
+    "Corporate Information",
+    "Legal Notices",
+  ],
+
+  ["Account", "Jobs", "Privacy", "Only on Netflix"],
+  ["Media Centre", "Terms of Use", "Contact Us"],
+];
+
+const FooterList = ({ list }) => {
+  return (
+    <ul className="list">
+      {list.map((li) => (
+        <li key={Math.random()}>{li}</li>
+      ))}
+    </ul>
+  );
+};
+
+export const Footer = () => { 
+  return (
+    <div className="footer">
+      <h5>Questions? <span>Call 000-800-040-1843</span></h5>
+      <div className="lists">
+        {links.map((list) => (
+          <FooterList key={Math.random()} list={list} />
+        ))}
+      </div>
+      <div className='ending'>
+      <Language  />
+      <p>Netflix India</p>
+      </div>
+      
+    </div>
+  );
+};
