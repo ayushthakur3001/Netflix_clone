cd8063cad4d4e18d55a996bb13b004acae798b82
Ayush <ayushthakur3001@gmail.com>
2021-09-14 15:43:46 +0530

    Hosted to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <HashRouter >
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
